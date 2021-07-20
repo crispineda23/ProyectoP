6630217ca454436a4a89b0417359b3a1ab1c14d7
crispineda23 <87692018+crispineda23@users.noreply.github.com>
2021-07-20 02:03:57 -0500

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>MI CAMIONCITO WEB</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <!-- BOOTSTRAP 4 -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+    <!-- FONT AWESOEM -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  </head>
+  <body>
+
+
+  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="interface.php">
+      <img src="images/truck.png" alt="" width="40" height="24" class="d-inline-block align-text-top">
+      MI CAMIONCITO WEB
+    </a>
+    <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="vehicles.php">
+    <img src="images/vehicle.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+    Vehiculos
+  </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="customer.php">
+    <img src="images/client.png" alt="" width="30" height="24" class="d-inline-block align-text-top"> 
+    Clientes
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="interface.php">
+    <img src="images/travel.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+    Viajes
+  </a>
+  </li>
+</ul>
+  </div>
+</nav>
+
+   
+
+
